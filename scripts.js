aec296aaf8fb4677171081cc0e8dac46e98abe60
Khamidullin Bulat <bulat.khamidullin.2015@gmail.com>
2020-02-29 15:48:08 +0300

    fix

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-$(".tag-button").click(function() {
+$(".open-btn").click(function() {
 	$(this).parent().children('.tags').toggleClass('disabled');
 	$(this).parent().children('.tags').toggleClass('enabled');
 	console.log(this);
